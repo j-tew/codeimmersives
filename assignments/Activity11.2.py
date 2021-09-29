440b5c46116ee86e16c07abef7d9ea399f406cb2
xFTWxOutlaw <justinrtew@gmail.com>
2021-09-29 12:31:25 -0500

    add 11.2

---
+++ assignments/Activity11.2.py
@@ -0,0 +1,36 @@
+from random import choice
+
+def rps():
+    choices = {
+    "R": "Rock",
+    "P": "Paper",
+    "S": "Scissors"
+    }
+    win = "You Win!"
+    lose = "You Lose!"
+    draw = "It's a tie!"
+    comp = choice(list(choices))
+    user = input(f"[R]ock, [P]aper, or [S]cissors?\n").upper()
+    selections = f"You chose:\n{choices[user]}\nComputer Chose:\n{choices[comp]}"
+    if user not in choices:
+        print("Invalid Selection")
+    elif user == comp:
+        print(selections, "\n", draw)
+    elif user == "R":
+        if comp == "S":
+            print(selections, "\n", win)
+        else:
+            print(selections, "\n", lose)
+    elif user == "P":
+        if comp == "R":
+            print(selections, "\n", win)
+        else:
+            print(selections, "\n", lose)
+    elif user == "S":
+        if comp == "P":
+            print(selections, "\n", win)
+        else:
+            print(selections, "\n", lose)
+
+rps()
+
