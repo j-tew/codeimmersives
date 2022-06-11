14edded0deb4fee1283851b006b2538c189eee9e
j-tew <justinrtew@gmail.com>
2022-06-11 00:26:02 -0500

    remove incomplete project

--- intro-django/random_API/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-# Account Model
-class Account(db.Model):
-    # Define the columns (primary key required)
-    account_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(25), unique=True, nullable=False)
-    last_name = db.Column(db.String(25), unique=True, nullable=False)
-    balance = db.Column(db.Float, unique=True, nullable=False)
